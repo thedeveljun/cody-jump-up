608adb78009e90c360e5079c530d7a0cffc0e89f
thedeveljun <thedeveljun@gmail.com>
2026-04-26 20:30:55 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'cody-jump-up-v1';
+const ASSETS = [
+  '/cody-jump-up/',
+  '/cody-jump-up/index.html',
+  '/cody-jump-up/manifest.json',
+  '/cody-jump-up/icon-192.png',
+  '/cody-jump-up/icon-512.png'
+];
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE_NAME).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE_NAME;}).map(function(k){return caches.delete(k);}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e){
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));
+});
